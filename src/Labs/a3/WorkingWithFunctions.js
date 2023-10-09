675742fbb8e5b0c83ece7ecf5b2707ff22f703b0
James <jamesflorez11@gmail.com>
2023-10-09 16:32:32 -0700

    Lab up to 2.2.7.8

--- src/Labs/a3/WorkingWithFunctions.js
+++ src/Labs/a3/WorkingWithFunctions.js
@@ -1,10 +1,14 @@
 import ES5Functions from "./ES5Functions";
 import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import FunctionParenthesisAndParameters from "./FunctionParenthesisAndParameters";
 function WorkingWithFunctions () {
     return (
         <div>
             <ES5Functions/>
             <ArrowFunctions/>
+            <ImpliedReturn/>
+            <FunctionParenthesisAndParameters/>
         </div>
     )
 }

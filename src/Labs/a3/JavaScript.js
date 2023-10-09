078bcc25c86f7000af4d0d5b75a604da7796e39c
James <jamesflorez11@gmail.com>
2023-10-09 16:58:22 -0700

    Lab up to 2.2.12

--- src/Labs/a3/JavaScript.js
+++ src/Labs/a3/JavaScript.js
@@ -5,6 +5,11 @@ import IfElse from "./IfElse";
 import TernaryOperator from "./TernaryOperator";
 import WorkingWithFunctions from "./WorkingWithFunctions";
 import WorkingWithArrays from "./WorkingWithArrays";
+import TemplateLiterals from "./TemplateLiterals";
+import House from "./House";
+import Spread from "./Spread";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
 function JavaScript() {
     console.log('Hello World!');
     return(
@@ -17,6 +22,11 @@ function JavaScript() {
             <TernaryOperator/>
             <WorkingWithFunctions/>
             <WorkingWithArrays/>
+            <TemplateLiterals/>
+            <House/>
+            <Spread/>
+            <Destructing/>
+            <FunctionDestructing/>
         </div>
     );
 }

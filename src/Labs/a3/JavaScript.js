b9eb2dd8820dd35f6f4275fd33d36fe9befa03a9
James <jamesflorez11@gmail.com>
2023-10-08 19:16:04 -0700

    Lab up to 2.2.5

--- src/Labs/a3/JavaScript.js
+++ src/Labs/a3/JavaScript.js
@@ -1,7 +1,18 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
 function JavaScript() {
+    console.log('Hello World!');
     return(
         <div>
             <h1>JavaScript</h1>
+            <VariablesAndConstants/>
+            <VariableTypes/>
+            <BooleanVariables/>
+            <IfElse/>
+            <TernaryOperator/>
         </div>
     );
 }

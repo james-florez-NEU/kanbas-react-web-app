2ddc78b8d272b73a418318f252a400f92ef5bce1
James <jamesflorez11@gmail.com>
2023-10-16 11:53:02 -0700

    Styled kanbas navigation except for icons

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,14 +1,18 @@
 import { Link, useLocation } from "react-router-dom";
+import "./kanbas_navigation.css";
 function KanbasNavigation() {
-    const links = ["Account", "Dashboard", "Courses", "Calendar"];
+    const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help"];
     const { pathname } = useLocation();
     return (
-        <div className="list-group" style={{ width: 150 }}>
+        <div className="list-group kanbas_navigation">
+            <div>
+                <img src="/kanbas/assets/NU_RGB_Notched-N_motto_RW.png" alt={"NEU Logo"}/>
+            </div>
             {links.map((link, index) => (
                 <Link
                     key={index}
                     to={`/Kanbas/${link}`}
-                    className={`list-group-item ${pathname.includes(link) && "active"}`}>
+                    className={`list-group-item kanbas_navigation_item ${pathname.includes(link) && "active"}`}>
                     {link}
                 </Link>
             ))}

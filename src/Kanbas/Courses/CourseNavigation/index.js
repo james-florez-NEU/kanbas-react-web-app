b38678581b0c2552572088291a393c70fb9fc70e
James <jamesflorez11@gmail.com>
2023-10-16 13:02:08 -0700

    Styled course navigation sidebar

--- src/Kanbas/Courses/CourseNavigation/index.js
+++ src/Kanbas/Courses/CourseNavigation/index.js
@@ -1,12 +1,13 @@
 import { Link, useParams, useLocation } from "react-router-dom";
+import "./course_navigation.css";
 
 
 function CourseNavigation() {
-    const links = ["Home", "Modules", "Assignments", "Grades"];
+    const links = ["Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", "Quizzes", "Grades", "People", "Panopto Video", "Discussions", "Announcements", "Pages", "Files", "Rubrics", "Outcomes", "Conferences", "Collaborations", "Syllabus", "Progress Reports (EAB Navigate)", "Settings"];
     const { courseId } = useParams();
     const { pathname } = useLocation();
     return (
-        <div className="list-group" style={{ width: 150 }}>
+        <div className="list-group course_navigation">
             {links.map((link, index) => (
                 <Link
                     key={index}

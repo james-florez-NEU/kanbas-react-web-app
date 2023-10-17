29ee0844cc44ae88ca92bb652e83eb4977552e83
James <jamesflorez11@gmail.com>
2023-10-17 12:02:49 -0700

    Basic implementation of modules screen

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -6,22 +6,60 @@ import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/AssignmentEditor";
 import Grades from "./Grades";
+import { FaGlasses } from "react-icons/fa";
+import { FaRegCheckCircle, FaEllipsisV, FaPlus, FaBars } from "react-icons/fa";
+import "./index.css";
+
 
 function Courses() {
     const { courseId } = useParams();
     const course = db.courses.find((course) => course._id === courseId);
     return (
         <div>
-            <h1>Course {course.name}</h1>
-            <CourseNavigation />
+            <div className="header">
+                <FaBars className="fa-bars"/>
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item"><a href="#">{course.number}</a></li>
+                        <li className="breadcrumb-item">></li>
+                        <li className="breadcrumb-item active" aria-current="page">Modules</li>
+                    </ol>
+                </nav>
+                <button className="btn btn-secondary float-end">
+                    <FaGlasses/> Student View
+                </button>
+            </div>
+
+            <hr/>
+
             <div>
-                <div
-                    className="overflow-y-scroll position-fixed bottom-0 end-0"
-                    style={{
-                        left: "320px",
-                        top: "50px",
-                    }}
-                >
+                <button className="btn btn-secondary float-end"><FaEllipsisV/></button>
+                <button className="btn btn-danger float-end">
+                    <FaPlus/>
+                    Module
+                </button>
+                <div className="dropdown float-end">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <FaRegCheckCircle/>
+                        Publish All
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="#">Action</a></li>
+                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+                <button className="btn btn-secondary float-end">View Progress</button>
+                <button className="btn btn-secondary float-end">Collapse All</button>
+                <br/>
+                <hr/>
+            </div>
+
+            <hr/>
+
+            <CourseNavigation/>
+            <div className="main-content">
+                <div className="overflow-y-scroll">
                     <Routes>
                         <Route path="/" element={<Navigate to="Home"/>} />
                         <Route path="Home" element={<Home/>} />

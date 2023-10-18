697b918a5aecd3c91689c658b5a77af4dc8c78d4
James <jamesflorez11@gmail.com>
2023-10-17 17:41:45 -0700

    A4 lab up to 2.4

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -3,6 +3,13 @@ import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 function Assignment4() {
     function sayHello() {
         alert("Hello");
@@ -11,6 +18,13 @@ function Assignment4() {
     return (
         <div>
             <h1>Assignment 4</h1>
+            <ParentStateComponent/>
+            <ArrayStateVariable/>
+            <ObjectStateVariable/>
+            <DateStateVariable/>
+            <StringStateVariables/>
+            <BooleanStateVariables/>
+            <Counter/>
             <EventObject/>
             <PassingFunctions theFunction={sayHello}/>
             <PassingDataOnEvent/>

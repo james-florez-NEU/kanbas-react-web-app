32d073ba3fde02040ad25e6a770dc6aeda2645a3
James <jamesflorez11@gmail.com>
2023-10-18 17:46:36 -0700

    A3: Use flex-fill to fill width of screen

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -8,7 +8,7 @@ function Kanbas() {
     return (
         <div className="d-flex">
             <KanbasNavigation />
-            <div>
+            <div className="flex-fill">
                 <Routes>
                     <Route path="/" element={<Navigate to="Dashboard" />} />
                     <Route path="Account" element={<h1>Account</h1>} />

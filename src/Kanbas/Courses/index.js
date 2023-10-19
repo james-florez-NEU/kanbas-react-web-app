5794c17ea55ea2d10fa575bc383702eac1543f48
James <jamesflorez11@gmail.com>
2023-10-19 11:53:09 -0700

    A3: Fix layout for courses

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -15,6 +15,8 @@ function Courses() {
     const course = db.courses.find((course) => course._id === courseId);
     return (
         <div>
+
+
             <div className="header">
                 <FaBars className="fa-bars"/>
                 <nav aria-label="breadcrumb">
@@ -24,50 +26,54 @@ function Courses() {
                         <li className="breadcrumb-item active" aria-current="page">Modules</li>
                     </ol>
                 </nav>
-                <button className="btn btn-secondary float-end">
+                <button className="btn btn-secondary float-end ms-2">
                     <FaGlasses/> Student View
                 </button>
             </div>
-
             <hr/>
 
-            <div>
-                <button className="btn btn-secondary float-end"><FaEllipsisV/></button>
-                <button className="btn btn-danger float-end">
-                    <FaPlus/>
-                    Module
-                </button>
-                <div className="dropdown float-end">
-                    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <FaRegCheckCircle/>
-                        Publish All
-                    </button>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-                <button className="btn btn-secondary float-end">View Progress</button>
-                <button className="btn btn-secondary float-end">Collapse All</button>
-                <br/>
-                <hr/>
-            </div>
+            <div className="flex-container">
+                <CourseNavigation/>
 
-            <hr/>
+                <div className="home-center-screen">
+                    <div>
+                        <button className="btn btn-secondary float-end"><FaEllipsisV/></button>
+                        <button className="btn btn-danger float-end">
+                            <FaPlus/>
+                            Module
+                        </button>
+                        <div className="dropdown float-end">
+                            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <FaRegCheckCircle/>
+                                Publish All
+                            </button>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </div>
+                        <button className="btn btn-secondary float-end">View Progress</button>
+                        <button className="btn btn-secondary float-end">Collapse All</button>
+                        <br/>
+                        <hr/>
+                    </div>
 
-            <CourseNavigation/>
-            <div className="main-content">
-                <div className="overflow-y-scroll">
-                    <Routes>
-                        <Route path="/" element={<Navigate to="Home"/>} />
-                        <Route path="Home" element={<Home/>} />
-                        <Route path="Modules" element={<Modules/>} />
-                        <Route path="Assignments" element={<Assignments/>} />
-                        <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>}/>
-                        <Route path="Grades" element={<Grades/>} />
-                    </Routes>
+                    <div className="main-content">
+                        <div className="overflow-y-scroll">
+                            <Routes>
+                                <Route path="/" element={<Navigate to="Home"/>} />
+                                <Route path="Home" element={<Home/>} />
+                                <Route path="Modules" element={<Modules/>} />
+                                <Route path="Assignments" element={<Assignments/>} />
+                                <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>}/>
+                                <Route path="Grades" element={<Grades/>} />
+                            </Routes>
+                        </div>
+                    </div>
                 </div>
+
+
             </div>
         </div>
     );

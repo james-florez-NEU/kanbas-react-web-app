21ac12746e2c06d969fc7fe0d41f81fc64699b8c
James <jamesflorez11@gmail.com>
2023-10-24 15:41:31 -0700

    A4: Completed lab

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -11,6 +11,7 @@ import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
 import ReduxExamples from "./ReduxExamples";
+import TodoList from "./ReduxExamples/todos/TodoList";
 function Assignment4() {
     function sayHello() {
         alert("Hello");
@@ -19,6 +20,7 @@ function Assignment4() {
     return (
         <div>
             <h1>Assignment 4</h1>
+            <TodoList/>
             <ReduxExamples/>
             <ParentStateComponent/>
             <ArrayStateVariable/>

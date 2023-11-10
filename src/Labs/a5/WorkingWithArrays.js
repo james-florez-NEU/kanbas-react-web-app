12edb7374d85b4c9d519a1b8b6ed895a232ae74f
James <jamesflorez11@gmail.com>
2023-11-10 12:54:39 -0800

    A5: Lab up to 3.5

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function WorkingWithArrays() {
     const API = "http://localhost:4000/a5/todos";
     const [todo, setTodo] = useState({
@@ -9,6 +9,34 @@ function WorkingWithArrays() {
         due: "2021-09-09",
         completed: false,
     });
+    const [todos, setTodos] = useState([]);
+    const fetchTodos = async () => {
+        const response = await axios.get(API);
+        setTodos(response.data);
+    };
+    const removeTodo = async (todo) => {
+        const response = await axios
+            .get(`${API}/${todo.id}/delete`);
+        setTodos(response.data);
+    };
+    const createTodo = async () => {
+        const response = await axios.get(`${API}/create`);
+        setTodos(response.data);
+    };
+    const fetchTodoById = async (id) => {
+        const response = await axios.get(`${API}/${id}`);
+        setTodo(response.data);
+    };
+    const updateTitle = async () => {
+        const response = await axios.get(
+            `${API}/${todo.id}/title/${todo.title}`);
+        setTodos(response.data);
+    };
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
     return (
         <div>
             <h3>Working with Arrays</h3>
@@ -36,6 +64,14 @@ function WorkingWithArrays() {
                className="btn btn-primary me-2">
                 Create Todo
             </a>
+            <button onClick={createTodo}
+                    className="btn btn-primary mb-2 w-100">
+                Create Todo
+            </button>
+            <button onClick={updateTitle}
+                    className="btn btn-success mb-2 w-100">
+                Update Title
+            </button>
             <input
                 value={todo.id}
                 onChange={(e) => setTodo({
@@ -43,6 +79,28 @@ function WorkingWithArrays() {
                 className="form-control mb-2"
                 type="number"
             />
+            <input value={todo.title}
+                   onChange={(e) => setTodo({ ...todo,
+                       title: e.target.value })} />
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <li key={todo.id}
+                        className="list-group-item">
+                        <button
+                            onClick={() => fetchTodoById(todo.id)}
+                            className="btn btn-warning me-2 float-end" >
+                            Edit
+                        </button>
+                        <button
+                            onClick={() => removeTodo(todo)}
+                            className="btn btn-danger float-end" >
+                            Remove
+                        </button>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
+
             <h3>Deleting from an Array</h3>
             <a href={`${API}/${todo.id}/delete`}
                className="btn btn-primary me-2">

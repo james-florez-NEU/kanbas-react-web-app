0eddc309be3e470bc6c6bb8761dabab1d62f15c9
James <jamesflorez11@gmail.com>
2023-11-22 12:22:36 -0800

    A6: Lab up to 3.5.3 (not working)

--- src/project/index.js
+++ src/project/index.js
@@ -1,18 +1,29 @@
 import Signin from "../users/signin";
-import { Routes, Route, Navigate } from "react-router-dom";
+import Account from "../users/account";
+import {Routes, Route, Navigate, Link} from "react-router-dom";
 import Nav from "../Nav";
 function Project() {
     return (
+        <div>
+        <Nav/>
         <div className="row">
             <div className="col-2">
-                <Nav />
+                <div className="list-group mt-2">
+                    <Link to="/project/home" className="nav-link">Home</Link>
+                    <Link to="/project/search" className="nav-link">Search</Link>
+                    <Link to="/project/signin" className="nav-link">Signin</Link>
+                    <Link to="/project/signup" className="nav-link">Signup</Link>
+                    <Link to="/project/account" className="nav-link">Account</Link>
+                </div>
             </div>
             <div className="col-10">
                 <Routes>
                     <Route path="/" element={<Navigate to="/project/home" />} />
                     <Route path="/signin" element={<Signin />} />
+                    <Route path="/account" element={<Account />} />
                 </Routes>
             </div>
+        </div>
         </div>
     );
 }

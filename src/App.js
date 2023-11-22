34997f5e9ec8e4eeea4bd8b595d5cb3d46b48cbe
James <jamesflorez11@gmail.com>
2023-11-22 11:34:52 -0800

    A6: Lab up to 3.5.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './index.css';
 import Labs from './Labs';
 import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
+import Project from "./project";
 import {HashRouter} from "react-router-dom";
 import {Routes, Route, Navigate} from "react-router";
 
@@ -12,6 +13,7 @@ function App() {
               <Routes>
                   <Route path="/"         element={<Navigate to="/Labs"/>}/>
                   <Route path="/hello"    element={<HelloWorld/>}/>
+                  <Route path="/project/*" element={<Project />} />
                   <Route path="/Labs/*"   element={<Labs/>}/>
                   <Route path="/Kanbas/*" element={<Kanbas/>}/>
               </Routes>

422e981d8276f5a0f027c69586a7e89178ab5d6f
James <jamesflorez11@gmail.com>
2023-12-06 16:39:19 -0800

    Weather: Add roles to registration

--- src/users/register.js
+++ src/users/register.js
@@ -4,7 +4,7 @@ import * as client from "./client";
 function Register() {
     const [error, setError] = useState("");
     const [credentials, setCredentials] = useState({
-        username: "", password: "" });
+        username: "", password: "", role: "USER" });
     const navigate = useNavigate();
     const signup = async () => {
         try {
@@ -18,16 +18,33 @@ function Register() {
         <div>
             <h1>Register</h1>
             {error && <div>{error}</div>}
+            <label for="username">Username</label>
             <input
+                id="username"
                 value={credentials.username}
                 onChange={(e) => setCredentials({
                     ...credentials,
                     username: e.target.value })} />
+            <br/>
+            <label for="password">Password</label>
             <input
+                id="password"
                 value={credentials.password}
                 onChange={(e) => setCredentials({
                     ...credentials,
                     password: e.target.value })} />
+            <br/>
+            <label for="role">Role</label>
+            <select
+                id="role"
+                onChange={(e) => setCredentials({
+                    ...credentials,
+                    role: e.target.value })}>
+                <option value="USER">User</option>
+                <option value="FORECASTER">Weather Forecaster</option>
+                <option value="MODERATOR">Moderator</option>
+            </select>
+
             <button onClick={signup}>
                 Signup
             </button>

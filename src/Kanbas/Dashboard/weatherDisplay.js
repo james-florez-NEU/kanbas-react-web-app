dea8ca0e801e3fbd7e96fd5eefc1e656780acae7
James <jamesflorez11@gmail.com>
2023-12-06 19:16:20 -0800

    Weather: Card for current weather

--- src/Kanbas/Dashboard/weatherDisplay.js
+++ src/Kanbas/Dashboard/weatherDisplay.js
@@ -19,13 +19,27 @@ const WeatherDisplay = () => {
 
     return (
         <div>
-            <h2>Weather Information</h2>
             {weatherData ? (
-                <div>
-                    <h3>Location: {weatherData.location.name}</h3>
-                    <p>Temperature: {weatherData.current.temp_c}°C</p>
-                    <p>Condition: {weatherData.current.condition.text}</p>
-                    {/* Add more weather details as needed */}
+                <div className="card">
+                    <div className="card-body">
+                        <img src={weatherData.current.condition.icon} alt="Weather Icon" className="float-end" />
+                        <h3 className="card-title">{weatherData.location.name}</h3>
+                        <p className="card-text">Temperature: {weatherData.current.temp_f}°F</p>
+                        <p className="card-text">Condition: {weatherData.current.condition.text}</p>
+                        <ul className="list-group list-group-flush">
+                            <li className={"list-group-item"}>Feels Like: {weatherData.current.feelslike_f}°F</li>
+                            <li className={"list-group-item"}>Precipitation: {weatherData.current.precip_in} in</li>
+                            <li className="list-group-item">Humidity: {weatherData.current.humidity}%</li>
+                            <li className={"list-group-item"}>Pressure: {weatherData.current.pressure_in} in</li>
+                            <li className={"list-group-item"}>UV Index: {weatherData.current.uv}</li>
+                            <li className={"list-group-item"}>Visibility: {weatherData.current.vis_miles} miles</li>
+                            <li className={"list-group-item"}>Cloud Cover: {weatherData.current.cloud} %</li>
+                            <li className="list-group-item">Wind Speed: {weatherData.current.wind_kph} km/h</li>
+                            <li className={"list-group-item"}>Wind Gust: {weatherData.current.gust_kph} km/h</li>
+                            <li className={"list-group-item"}>Wind Direction: {weatherData.current.wind_dir}</li>
+                            <li className={"list-group-item"}>Wind Degree: {weatherData.current.wind_degree}°</li>
+                        </ul>
+                    </div>
                 </div>
             ) : (
                 <p>Loading weather data...</p>
